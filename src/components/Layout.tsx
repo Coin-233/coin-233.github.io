5b28c3dcca8692c614d17298ab6341e2aa8def94
Coin-233 <coin233mail@gmail.com>
2025-07-06 07:39:18 +0800

    chore: copyright

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,7 +15,8 @@ function Layout({ children }: LayoutProps) {
             </main>
             <footer>
                 Last Update 2025-04-28<br />
-                <IoIosGitBranch /> {import.meta.env.VITE_COMMIT_ID}
+                <IoIosGitBranch /> {import.meta.env.VITE_COMMIT_ID} <br/><br/>
+                <span title="Copyright 2025 All rights reserved">©2025 保留所有右边</span>
             </footer>
         </div>
     )
